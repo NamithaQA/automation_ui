a92419a017187ea756185ed93c8066e6d6227d6c
Namitha Jalagani <namithaj1994+challenge@gmail.com>
2026-07-05 22:38:38 +0400

    Add test step to check nav bar is visible

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -1,4 +1,6 @@
 import pytest
+
+from base_page import BasePage
 from pages.home_page import HomePage
 
 
@@ -9,14 +11,14 @@ from pages.home_page import HomePage
                                      # ('mbg', '' )
                                      ])
 def test_navigation(page, config, nav_url):
+    """Test Nav bar exists, all menu items are visible,nav items are inked correctly,  """
     home = HomePage(page)
     home.goto(config["base_url"])
     assert page.url == config["base_url"] + "en-AE", "Home page opened is wrong"
+
+    home.assert_visible("//nav[@aria-label='Main']")
 
     home.assert_visible(f"(//a[@href='/en-AE{nav_url}'])[1]")
     home.click(f"(//a[@href='/en-AE{nav_url}'])[1]")
     page.wait_for_url(f"**{nav_url}")
     assert page.url == config["base_url"] + "en-AE" + nav_url.lower(), "Nav page navigated is wrong"
-
-
-
