3ff64bbfe174a05cfa3167c8010428a7a8674d42
Namitha Jalagani <namithaj1994+challenge@gmail.com>
2026-07-05 22:26:37 +0400

    Paramterize the navigation test and add the assertions

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -1,23 +1,22 @@
 import pytest
-
 from pages.home_page import HomePage
-from pages.base_page import BasePage
 
 
-@pytest.mark.parametrize(("nav_name", "url"), [('Explore', '/explore'),
-                                             ('Features', '/features'),
-                                            ('Otc Desk', '/features/otc_desk'),
-                                             ('Company', '/company'),
-                                             ('Support', '/support'),
-                                             # ('mbg', '' )
-                                             ])
-def test_navigation(page, config, nav_name, url):
+@pytest.mark.parametrize("nav_url", ['/explore',
+                                     '/features',
+                                     '/features/otc-desk',
+                                     '/company',
+                                     # ('mbg', '' )
+                                     ])
+def test_navigation(page, config, nav_url):
     home = HomePage(page)
     home.goto(config["base_url"])
-    assert page.url == config["base_url"]+"en-AE", "Home page opened is wrong"
+    assert page.url == config["base_url"] + "en-AE", "Home page opened is wrong"
 
-    BasePage(page).assert_visible(f"(//a[@href='/en-AE{url}'])[1]")
-    BasePage(page).click(f"(//a[@href='/en-AE{url}'])[1]")
-    BasePage(page).assert_url_contains(config["base_url"] + "en-AE" + url.lower())
+    home.assert_visible(f"(//a[@href='/en-AE{nav_url}'])[1]")
+    home.click(f"(//a[@href='/en-AE{nav_url}'])[1]")
+    page.wait_for_url(f"**{nav_url}")
+    assert page.url == config["base_url"] + "en-AE" + nav_url.lower(), "Nav page navigated is wrong"
+
 
 
