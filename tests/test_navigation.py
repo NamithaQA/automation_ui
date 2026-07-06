45fb5d0085e97b4de58c22d0f3dee68a492f7687
Namitha Jalagani <namithaj1994+challenge@gmail.com>
2026-07-06 19:05:44 +0400

    Uopdated navigation with MBG check

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -1,23 +1,27 @@
 import pytest
-
+from playwright.sync_api import expect
 from pages.home_page import HomePage
 
 
-@pytest.mark.parametrize("nav_url", ['/explore',
-                                     '/features',
-                                     '/features/otc-desk',
-                                     '/company',
-                                     # ('mbg', '' )
-                                     ])
-def test_navigation(page, config, nav_url):
-    """Test Nav bar exists, all menu items are visible,nav items are inked correctly,  """
+@pytest.mark.parametrize(("nav", "nav_url"), [
+    ('Explore', '/explore'),
+    ('Features', '/features'),
+    ('OTC Desk', '/features/otc-desk'),
+    ('Company', '/company'),
+    ('$MBG', '/token.mb.io')
+])
+def test_navigation(page, config, nav, nav_url):
+    """Test Nav bar exists, all menu items are visible, nav items are linked correctly"""
     home = HomePage(page)
     home.goto(config["base_url"])
-    assert page.url == config["base_url"] + "en-AE", "Home page opened is wrong"
+    expect(page).to_have_url(config["base_url"] + "en-AE")
+    expect(page.get_by_role("navigation", name="Main")).to_be_visible()  # Check Nav is visible
 
-    home.assert_visible("//nav[@aria-label='Main']")
-
-    home.assert_visible(f"(//a[@href='/en-AE{nav_url}'])[1]")
-    home.click(f"(//a[@href='/en-AE{nav_url}'])[1]")
-    page.wait_for_url(f"**{nav_url}")
-    assert page.url == config["base_url"] + "en-AE" + nav_url.lower(), "Nav page navigated is wrong"
+    if nav == "$MBG":  # MBG nav item opens a new tab
+        with page.expect_popup() as mbg_page:
+            page.get_by_role("link", name=nav, exact=True).first.click()
+        assert nav_url in mbg_page.value.url, "MBG redirected to wrong URL"
+    else:
+        expect(page.get_by_role('link', name=nav).first).to_be_visible()  # Nav items are visible
+        page.get_by_role('link', name=nav).first.click()  # Click Nav item
+        expect(page).to_have_url(config["base_url"] + "en-AE" + nav_url)  # Verify nav item redirection
